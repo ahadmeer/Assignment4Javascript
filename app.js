b432eb4a2d0772884aeda37722e424cd6e6220f7
ahadmeer <abdulahadmeer97@gmail.com>
2022-05-13 14:59:55 -0700

    assignment4

---
+++ app.js
@@ -0,0 +1,45 @@
+// Q:1
+// var number = 3.45214
+// document.write("number :" , number , "<br>")
+// var round = Math.round (number);
+// document.write("round of value :" , round , "<br>")
+// var floor = Math.floor (number);
+// document.write("floor value : " , floor , "<br>")
+// var ceil = Math.ceil (number);
+// document.write("ceil value : " , ceil)
+
+
+// Q:2
+// var number = -2.673
+// document.write("number : " , number , "<br>")
+// var round = Math.round (number);
+// document.write("round of value :" , round , "<br>")
+// var floor = Math.floor (number);
+// document.write("floor of value  : " , floor , "<br>")
+// var ceil = Math.ceil (number);4
+// document.write("ceil of value :" , ceil)
+
+
+
+// Q:3
+
+// var floor = Math.floor (-3.23);
+// var ceil = Math.ceil(3.23);
+// document.write("the absolute value of " , floor , " is " , ceil) 
+
+
+
+// Q:4
+// var a = Math.floor(Math.random () * 10 ) + 1;
+// document.write("random dice value : " , a)
+
+
+// Q:5
+// var a = Math.floor(Math.random () * 100 ) + 1;
+// document.write(a)
+
+function functionName(fname = "abdul", lname = "ahad"){
+    alert("Hello" + fname + lname);
+
+}
+functionName()
